e9eae799c1f92392f6d29516f06fd5fc441dcb22
PappBeata <pappbeata@gmail.com>
2017-01-26 20:45:01 +0100

    Add percent of daily CH need to the meal type class

--- project/src/main/java/com/chcounter/models/Type.java
+++ project/src/main/java/com/chcounter/models/Type.java
@@ -9,21 +9,24 @@ import javax.persistence.Table;
  * Created by regnisalram on 1/2/17.
  */
 @Entity
-@Table (name = "meal_type")
+@Table(name = "meal_type")
 public class Type {
 
     @Id
     public String code;
     public String description;
+    public float percentOfDaily;
 
     @OneToOne(mappedBy = "typeOfFood")
     public Meal meal;
 
-    Type() {}
+    Type() {
+    }
 
-    public Type(String code, String description) {
+    public Type(String code, String description, float percentOfDaily) {
         this.code = code;
         this.description = description;
+        this.percentOfDaily = percentOfDaily;
     }
 
     public String getDescription() {
